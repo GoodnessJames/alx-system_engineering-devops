875dbba311a7c25e2c507548c689cc2c0cc24cff
GoodnessJames <akomagoodness97@gmail.com>
2023-10-30 16:19:38 +0100

    Puppet ssh config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,15 @@
-#!/usr/bin/env bash
 # Modify the configuration file ssh_config
 
 file { '/etc/ssh/ssh_config':
   ensure => present,
+}
 
 file_line { 'Use private key in ~/.ssh/school':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',
-  match => '^#PasswordAuthentication',
+}
 
 file_line { 'Disable password Authentication':
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
-  match => '^#IdentityFile',
 }
